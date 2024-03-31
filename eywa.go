3bfaef17336294ab9abb9a55e72c6e264381c1bd
Divi <divyansh@hasura.io>
2024-03-31 10:31:31 +0530

    try builder and option patterns

--- eywa.go
+++ eywa.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"reflect"
 	"strings"
@@ -23,8 +24,95 @@ func NewClient(endpoint string, headers map[string]string) *Client {
 	}
 }
 
+func (c *Client) do(q string) (*bytes.Buffer, error) {
+	reqObj := graphqlRequest{
+		Query: q,
+	}
+
+	var reqBytes bytes.Buffer
+	err := json.NewEncoder(&reqBytes).Encode(&reqObj)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, c.endpoint, &reqBytes)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	for key, value := range c.headers {
+		req.Header.Add(key, value)
+	}
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var respBytes bytes.Buffer
+	_, err = io.Copy(&respBytes, resp.Body)
+	return &respBytes, err
+}
+
 type Model interface {
 	ModelName() string
+}
+
+type query[T Model] struct {
+	operationName string
+	selectFields  []string
+	whereClause   string
+	limitClause   string
+	orderByClause string
+
+	model T
+}
+
+func Query[T Model](model T, opts ...queryOption[T]) *query[T] {
+	q := &query[T]{
+		operationName: "Get",
+		model:         model,
+	}
+	for _, opt := range opts {
+		opt(q)
+	}
+	return q
+}
+
+func (q *query[T]) Select(fields []string) *query[T] {
+	q.selectFields = fields
+	return q
+}
+
+func (q *query[T]) Exec(client *Client) ([]T, error) {
+	queryFormat := "query %s {%s {%s}}"
+	respBytes, err := client.do(fmt.Sprintf(queryFormat, q.operationName, q.model.ModelName(), strings.Join(q.selectFields, "\n")))
+
+	type graphqlResponse struct {
+		Data   map[string][]T `json:"data"`
+		Errors []graphqlError `json:"errors"`
+	}
+
+	respObj := graphqlResponse{}
+
+	err = json.NewDecoder(respBytes).Decode(&respObj)
+	if err != nil {
+		return nil, err
+	}
+	return respObj.Data[q.model.ModelName()], nil
+}
+
+type queryOption[T Model] func(*query[T])
+
+func WithOperationName[T Model](oprnName string) queryOption[T] {
+	return func(q *query[T]) {
+		q.operationName = oprnName
+	}
+}
+
+func SelectFields[T Model](fields []string) queryOption[T] {
+	return func(q *query[T]) {
+		q.selectFields = fields
+	}
 }
 
 type graphqlRequest struct {
--- eywa_test.go
+++ eywa_test.go
@@ -30,3 +30,37 @@ func TestSelect(t *testing.T) {
 		assert.Equal(t, expectedResp, resp)
 	}
 }
+
+func TestQuery(t *testing.T) {
+	accessKey := os.Getenv("TEST_HGE_ACCESS_KEY")
+	s := testTable{}
+	c := NewClient("https://aware-cowbird-80.hasura.app/v1/graphql",
+		map[string]string{
+			"x-hasura-access-key": accessKey,
+		},
+	)
+
+	resp, err := Query(&s).Select([]string{"name"}).Exec(c)
+	expectedResp := []*testTable{{Name: "abcdefgh"}}
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedResp, resp)
+	}
+}
+
+func TestQueryWithOptions(t *testing.T) {
+	accessKey := os.Getenv("TEST_HGE_ACCESS_KEY")
+	s := testTable{}
+	c := NewClient("https://aware-cowbird-80.hasura.app/v1/graphql",
+		map[string]string{
+			"x-hasura-access-key": accessKey,
+		},
+	)
+
+	resp, err := Query(&s, SelectFields[*testTable]([]string{"name"})).Exec(c)
+	expectedResp := []*testTable{{Name: "abcdefgh"}}
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedResp, resp)
+	}
+}
